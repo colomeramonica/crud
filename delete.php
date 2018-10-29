df66b291ae7f55d314f12ae19f46b97fca8493fa
Mônica Colomera <colomeramonica@gmail.com>
2018-10-28 21:49:39 -0300

    first commit

--- delete.php
+++ delete.php
@@ -1,41 +1,36 @@
 <?php
-// Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
-    // Include config file
-    require_once "connect.php";
-    
-    // Prepare a delete statement
-    $sql = "DELETE FROM `funcionarios` WHERE id_funcionario = ?";
-    
-    if($stmt = $connection->prepare($sql)){
-        // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("i", $param_id);
-        
-        // Set parameters
-        $param_id = trim($_POST["id"]);
-        
-        // Attempt to execute the prepared statement
-        if($stmt->execute()){
-            // Records deleted successfully. Redirect to landing page
-            header("location: index.php");
-            exit();
-        } else{
-            echo "Oops! Something went wrong. Please try again later.";
+
+if (isset($_POST['id']) && !empty($_POST['id'])) {
+    require_once('connect.php');
+
+        $sql = 'DELETE FROM `produto` WHERE `id` = ?';
+
+        $stmt = $connection->prepare($sql);
+
+        if ($stmt) {
+            $stmt->bind_param('i', $paramId);
+
+            $paramId = trim($_POST['id']);
+
+            if ($stmt->execute()) {
+                header('location: index.php');
+            } else {
+                echo 'Erro ao salvar.';
+            }
+
         }
-    }
-     
-    // Close statement
+            
     $stmt->close();
-    
-    // Close connection
+
     $connection->close();
 }
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>View Record</title>
+    <title>Remover</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
@@ -50,15 +45,15 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h1>Delete Record</h1>
+                        <h1>Apagar registro</h1>
                     </div>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="alert alert-danger fade in">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to delete this record?</p><br>
+                    <form method="post">
+                        <div class="alert alert-danger fade-in">
+                            <p>Você tem certeza que deseja remover?</p>
                             <p>
-                                <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-default">No</a>
+                                <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>">
+                                <input type="submit" value="Sim" class="btn btn-danger">
+                                <a href="index.php" class="btn btn-default">Não</a>
                             </p>
                         </div>
                     </form>
